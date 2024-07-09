ddb9c17ee29b5d42e964919a0faab9a95992db07
Lengthrequired <hyp3rcr4ft@gmail.com>
2024-07-09 21:35:17 +0200

    I try some wizardy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+#install dependencies
+sudo apt-get install nodejs
+sudo apt-get install npm
+
+#install node modules
+npm install
+
+#build the project
+npm run build
+
+#mv the build folder to the good folder (that is outside the docker container)
+mv build ../
+cd ../
+pwd
+
+#THIS IS A TEST BTW
