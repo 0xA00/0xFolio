ec43832bb7e3dcb202f2632cef6e6387eec9d62b
Lengthrequired <hyp3rcr4ft@gmail.com>
2024-07-09 02:01:18 +0200

    push mania

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,7 @@ import UsrNimg from "$lib/UsrNimg.svelte";
 
 <svelte:head>
 	<title>0xa0 Portfolio</title>
-	<meta name="description" content="0xa0's portfolio">
+	<meta name="description" content="0xa0's portfolio, I mainly do Minecraft related things and I try to learn new things :3">
 	<meta name="keywords" content="0xa0, 0xa0 website, 0xa0 projects">
 	<!-- have a big ass gif of a spinning blahaj-->
 	<meta name="og:image" content="https://web.0xa0.dev/blahaj.webp">
