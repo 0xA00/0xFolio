d448e13e9a3d2af42f5d0c81b14048797bd08d53
Lengthrequired <hyp3rcr4ft@gmail.com>
2024-07-22 22:51:10 +0200

    yes

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+import { page } from '$app/stores'
+</script>
+
+
+<h1>
+   {$page.status} :  Oops! Something went wrong. 
+</h1>
+<p>
+    What did you tried to do?
+</p>
+
+<img src="IKW.jpg" alt="IKW" width="200" height="200">
