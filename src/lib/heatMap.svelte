9467c13e2312f8cae9107caca7cdfaa7db1413c2
Lengthrequired <hyp3rcr4ft@gmail.com>
2024-07-31 23:13:55 +0200

    hetmap

--- src/lib/heatMap.svelte
+++ src/lib/heatMap.svelte
@@ -2,28 +2,31 @@
     .heatmap {
         display: flex;
         flex-direction: row;
-        gap: 1rem;
+        gap: 0.5rem;
         width: 100%;
         max-width: 100%;
         margin: auto;
+        padding-bottom: 2rem;
     }
 
     .dayoftheweek {
         display: flex;
         flex-direction: column;
-        gap: 1rem;
         justify-content: space-between;
+        font-size: 0.75rem;
+        padding-top:1.6rem;
+        padding-bottom:0.75rem;
     }
 
     .dayoftheweek div {
         font-weight: 500;
-        font-size: 1.1em;
+  
     }
 
     .monthandmap {
         display: flex;
         flex-direction: column;
-        gap: 1rem;
+      
     }
 
     .monthname {
@@ -31,17 +34,29 @@
         flex-direction: row;
         gap: 1rem;
         justify-content: space-between;
+        font-size: 0.75rem;
+        padding-right: 2.7rem;
     }
 </style>
 
 <div class="heatmap">
+    <div class="dayoftheweek">
+
+        <div>Mon</div>
+        <div></div>
+        <div>Wed</div>
+        <div></div>
+        <div>Fri</div>
+
+    </div>
     
     <div class="monthandmap">
         <div class="monthname">
-    
         </div>
         <div class="map">
-            
+            {#each Array(368) as _}
+            <div class="day"></div>
+            {/each}
         </div>
     </div>
 
@@ -93,48 +108,65 @@ const client = new GraphQLClient(endpoint, {
     }
 })
 
+const monthss = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+//take the month we are right now
+const date = new Date()
+const month = date.getMonth()
+console.log(month)
+//from the month that we are , create an array with the months that we have to show, so this one and all 11 before in monthss , it's an array of 12 months
+const months = [...monthss.slice(month + 1), ...monthss.slice(0, month + 1)]
+
+
+
+
 
 
 
 onMount(() => {
+    document.querySelector('.monthname').innerHTML = months.map(month => `<div>${month}</div>`).join('')
     client.request(github, varia).then(data => {
-        console.log(data)
-        
-        const weeks = data.user.contributionsCollection.contributionCalendar.weeks
-        const heatmap = document.querySelector('.map')
-        weeks.forEach(week => {
-            week.contributionDays.forEach(day => {
-                const dayElement = document.createElement('div')
-                dayElement.classList.add('day')
-                //add css here cuz it don't want to work in the style tag
-                //if data-count is between 1 and 9, the color will be --lowcommit
-                if(day.contributionCount >= 1 && day.contributionCount <= 9){
-                    dayElement.style.backgroundColor = 'var(--lowcommit)'
-                }
-                //if data-count is between 10 and 19, the color will be --mediumcommit
-                if(day.contributionCount >= 10 && day.contributionCount <= 19){
-                    dayElement.style.backgroundColor = 'var(--medcommit)'
-                }
-                //if data-count is between 20 and 29, the color will be --highcommit
-                if(day.contributionCount >= 20 && day.contributionCount <= 29){
-                    dayElement.style.backgroundColor = 'var(--highcommit)'
-                }
-                //if data-count is between 30 and 39, the color will be --veryhighcommit
-                if(day.contributionCount >= 30){
-                    dayElement.style.backgroundColor = 'var(--maxcommit)'
-                }
 
+       //redo the same thing but instead of creating the div , query the div and change the background color
+         const Alldays : any  = document.querySelectorAll('.day')
+            let i = 0;
+            data.user.contributionsCollection.contributionCalendar.weeks.forEach(week => {
+                week.contributionDays.forEach(day => {
+                    //add css here cuz it don't want to work in the style tag
+                    //if data-count is between 1 and 9, the color will be --lowcommit
+                    if(day.contributionCount >= 1 && day.contributionCount <= 9){
+                        Alldays[i].style.backgroundColor = 'var(--lowcommit)'
+                    }
+                    //if data-count is between 10 and 19, the color will be --mediumcommit
+                    if(day.contributionCount >= 10 && day.contributionCount <= 19){
+                        Alldays[i].style.backgroundColor = 'var(--medcommit)'
+                    }
+                    //if data-count is between 20 and 29, the color will be --highcommit
+                    if(day.contributionCount >= 20 && day.contributionCount <= 29){
+                        Alldays[i].style.backgroundColor = 'var(--highcommit)'
+                    }
+                    //if data-count is between 30 and 39, the color will be --veryhighcommit
+                    if(day.contributionCount >= 30){
+                        Alldays[i].style.backgroundColor = 'var(--maxcommit)'
+                    }
+                    //add a div below the day div with the data-count , this will be the number of commits , you need to hover the day to see it
+                    // Add a hovercard below the day div with the data-count
+                    if(day.contributionCount > 0){
+                    const hovercards = document.createElement('div');
+                    hovercards.classList.add('hovercard');
+                    hovercards.textContent = day.contributionCount + ' commits on ' + day.date;
+                    //change opacity to 0
+                    Alldays[i].appendChild(hovercards);
+                    }
 
-                dayElement.style.width = '10px'
-                dayElement.style.height = '10px'         
-                dayElement.setAttribute('data-count', day.contributionCount)
-                heatmap.appendChild(dayElement)
+                    i++;
+                })
             })
-        })
+    }) 
+    //
 
-        
-        heatmap.style.gridTemplateColumns = `repeat(${weeks.length}, 1fr)`
-    })
+
+
 })
 
 
