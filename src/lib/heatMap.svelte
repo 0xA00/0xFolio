f300acb4de37f70c35aba5b43b5d94d94a23d41c
0xa0 <hyp3rcr4ft@gmail.com>
2024-07-31 16:26:00 +0200

    changed color and added heatmap (not finished yet)

---
+++ src/lib/heatMap.svelte
@@ -0,0 +1,141 @@
+<style>
+    .heatmap {
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        width: 100%;
+        max-width: 100%;
+        margin: auto;
+    }
+
+    .dayoftheweek {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        justify-content: space-between;
+    }
+
+    .dayoftheweek div {
+        font-weight: 500;
+        font-size: 1.1em;
+    }
+
+    .monthandmap {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+
+    .monthname {
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        justify-content: space-between;
+    }
+</style>
+
+<div class="heatmap">
+    
+    <div class="monthandmap">
+        <div class="monthname">
+    
+        </div>
+        <div class="map">
+            
+        </div>
+    </div>
+
+
+</div>
+
+
+*Not finished yet , i need to add month and days of the week, but still want to show it to you, so you can see the progress*
+
+
+
+<script lang="ts">
+    //let's create a github contribution heatmap
+    //use github graphl API
+import { gql, GraphQLClient } from 'graphql-request'
+	import { onMount } from 'svelte';
+    import "./heatmap.css";
+
+
+const github = gql`
+query($userName:String!) { 
+  user(login: $userName){
+    contributionsCollection {
+      contributionCalendar {
+        totalContributions
+        weeks {
+          contributionDays {
+            contributionCount
+            date
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+
+const varia = {
+    userName: "0xA00"
+}
+
+const endpoint = "https://api.github.com/graphql"
+
+const client = new GraphQLClient(endpoint, {
+    headers: {
+        //I don't give a damn about this token, it's only a read token, what will you do with that ? know what public commit I have made ?
+        authorization: `Bearer `+ import.meta.env.VITE_githubtoken
+    }
+})
+
+
+
+
+onMount(() => {
+    client.request(github, varia).then(data => {
+        console.log(data)
+        
+        const weeks = data.user.contributionsCollection.contributionCalendar.weeks
+        const heatmap = document.querySelector('.map')
+        weeks.forEach(week => {
+            week.contributionDays.forEach(day => {
+                const dayElement = document.createElement('div')
+                dayElement.classList.add('day')
+                //add css here cuz it don't want to work in the style tag
+                //if data-count is between 1 and 9, the color will be --lowcommit
+                if(day.contributionCount >= 1 && day.contributionCount <= 9){
+                    dayElement.style.backgroundColor = 'var(--lowcommit)'
+                }
+                //if data-count is between 10 and 19, the color will be --mediumcommit
+                if(day.contributionCount >= 10 && day.contributionCount <= 19){
+                    dayElement.style.backgroundColor = 'var(--medcommit)'
+                }
+                //if data-count is between 20 and 29, the color will be --highcommit
+                if(day.contributionCount >= 20 && day.contributionCount <= 29){
+                    dayElement.style.backgroundColor = 'var(--highcommit)'
+                }
+                //if data-count is between 30 and 39, the color will be --veryhighcommit
+                if(day.contributionCount >= 30){
+                    dayElement.style.backgroundColor = 'var(--maxcommit)'
+                }
+
+
+                dayElement.style.width = '10px'
+                dayElement.style.height = '10px'         
+                dayElement.setAttribute('data-count', day.contributionCount)
+                heatmap.appendChild(dayElement)
+            })
+        })
+
+        
+        heatmap.style.gridTemplateColumns = `repeat(${weeks.length}, 1fr)`
+    })
+})
+
+
+</script>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,13 +1,99 @@
+<script>
+	import HeatMap from "$lib/heatMap.svelte";
+
+</script>
+
+
+
+
+
+
+
 <h1>About me</h1>
 
 
+<div class="userImg">
+    <img src="https://avatars.githubusercontent.com/0xa00" alt="avatar" width="200" height="200" />
+<div class="userName">
+    <div id="sentenceuser"><h2>Hi ! I'm <span id="user">0xa0</span> !</h2><h4>She/her</h4></div>
+    <h3>{~~(new Date().getFullYear()-new Date(2002, 11, 7)/31557600000)-1970} yrs old French developer</h3>
+    <h3>working as an Intern at Sanofi</h3>
+</div>
+</div>
+
+<p>hi ! I'm 0xa0, a  {~~(new Date().getFullYear()-new Date(2002, 11, 7)/31557600000)-1970} year old developer and I mainly do Minecraft related things and I try to learn new things.</p>
+<p>I live in France but I'm okay with moving to another country if I have to, I'm currently finishing my last year of computer science at the University of Montpellier-Sète.</p>
+
+
+<HeatMap/>
 
 
 
 
 <style>
+
+
+    
+
+
+    #sentenceuser{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 0.55rem;
+        }
+
+    #user{
+        color: var(--primary-color);
+
+        }
+
+    p{
+
+        text-wrap:pretty;  
+        font-weight: 500;
+        font-size: 1.1em;
+    }
+    .userImg{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 1rem;
+        transition: flex-direction 0.5s;
+        }
+
+    .userName{
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        }
+
+        .userName *{
+            margin: 0;
+            padding: 0;
+        }
     h1{
         color: var(--primary-color);
         font-size: 2rem;
         }
+
+   .userImg > img{
+        border-radius: 15%;
+        margin-top: 1rem;
+
+
+        margin-left: 1rem;
+        }
+
+    @media (max-width: 768px){
+
+        .userImg{
+            flex-direction: column;
+            transition: flex-direction 0.5s;
+            
+            gap: 1rem;
+            }
+
+
+    }
 </style>
